8cc7d3fb1d9b61efdc8af646aea62d60bf757429
fenerlidenizz-cpu <fenerlidenizz@gmail.com>
2026-08-27 13:52:33 +0200

    Implement caching strategies in service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,32 @@
+const CACHE_NAME = 'wohnung-pwa-v1';
+const ASSETS = [
+  '/Wohnung/',
+  '/Wohnung/index.html',
+  '/Wohnung/manifest.json'
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
